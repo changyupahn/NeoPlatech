1bd27274967e1eb5c13015b5b3e284b3b3222f20
GIGABYTE-P25 <GIGABYTE-P25@DESKTOP-0449D1S>
2019-02-20 18:18:36 +0900

    수정

--- NeoPlatech/src/main/java/boassoft/service/impl/AddressServiceImple.java
+++ NeoPlatech/src/main/java/boassoft/service/impl/AddressServiceImple.java
@@ -4,32 +4,33 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
-import egovframework.rte.fdl.cmmn.EgovAbstractServiceImpl;
 import boassoft.mapper.AddressMapper;
 import boassoft.service.AddressService;
 import boassoft.util.CommonList;
 import boassoft.util.CommonMap;
+import egovframework.rte.fdl.cmmn.EgovAbstractServiceImpl;
 
 @Service("addressService")
-public class AddressServiceImple extends EgovAbstractServiceImpl implements AddressService {
+public class AddressServiceImple extends EgovAbstractServiceImpl implements
+		AddressService {
 
-	@Resource(name="AddressMapper")
-    private AddressMapper addressMapper;
-	
-	public int insertAddress(CommonMap cmap) throws Exception{
-    	return addressMapper.insertAddress(cmap);
+	@Resource(name = "AddressMapper")
+	private AddressMapper addressMapper;
+
+	public int insertAddress(CommonMap cmap) throws Exception {
+		return addressMapper.insertAddress(cmap);
 	}
-	
-	public void deleteAddress(CommonMap cmap) throws Exception{
+
+	public void deleteAddress(CommonMap cmap) throws Exception {
 		addressMapper.deleteAddress(cmap);
 	}
-	
+
 	public CommonList getAddressList(CommonMap cmap) throws Exception {
 		CommonList list = addressMapper.getAddressList(cmap);
 		list.totalRow = addressMapper.getAddressListCnt(cmap);
 		return list;
 	}
-	
+
 	public CommonMap getAddressInfo(CommonMap cmap) throws Exception {
 		return addressMapper.getAddressInfo(cmap);
 	}
