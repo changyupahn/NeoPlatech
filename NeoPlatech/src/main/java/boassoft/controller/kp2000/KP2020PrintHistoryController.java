f10fefd98800a3a5905fe1955cc9b41a4f0f1ff7
GIGABYTE-P25 <GIGABYTE-P25@DESKTOP-0449D1S>
2019-03-04 19:04:18 +0900

    수정

--- NeoPlatech/src/main/java/boassoft/controller/kp2000/KP2020PrintHistoryController.java
+++ NeoPlatech/src/main/java/boassoft/controller/kp2000/KP2020PrintHistoryController.java
@@ -9,8 +9,6 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import boassoft.service.AssetHistoryService;
 import boassoft.service.PrintHistoryService;
 import boassoft.service.UserService;
 import boassoft.util.CamelUtil;
@@ -36,8 +34,10 @@ public class KP2020PrintHistoryController {
     @RequestMapping(value="/kp2000/kp2020.do")
 	public String kp2020(HttpServletRequest request, HttpServletResponse response, ModelMap model) throws Exception {
     	CommonMap cmap = new CommonMap(request);
+    	int pageLimit = (cmap.getInt("page", 1) - cmap.getInt("pageIdx", 1)) * cmap.getInt("pageSize", 50) ;
     	cmap.put("pageIdx", cmap.getString("pageIdx", "1"));
     	cmap.put("pageSize", cmap.getString("pageSize", "50"));
+    	cmap.put("pageLimit", pageLimit);     	
 
     	//검색값 유지
     	model.addAttribute("cmRequest",cmap);
@@ -50,7 +50,14 @@ public class KP2020PrintHistoryController {
     	CommonMap cmap = new CommonMap(request);
     	cmap.put("dataOrder", CamelUtil.deconvert2CamelCase(cmap.getString("dataOrder")));
     	cmap.put("dataOrderArrow", cmap.getString("dataOrderArrow"));
+    	cmap.put("pageLimit", cmap.getInt("pageLimit",1));
     	cmap.put("sAssetDiv", "1");
+    	
+    	System.out.println(" dataOrder " + "  : " + CamelUtil.deconvert2CamelCase(cmap.getString("dataOrder")));
+    	System.out.println(" dataOrderArrow " + "  : " + cmap.getString("dataOrderArrow"));
+    	System.out.println(" pageSize " + "  : " + cmap.getInt("pageSize"));
+    	System.out.println(" pageIdx " + "  : " + cmap.getInt("pageIdx"));
+    	System.out.println(" pageLimit " + "  : " + cmap.getInt("pageLimit"));
 
     	//그리드 세션 체크 및 메뉴 권한 설정
     	CommonMap gridSessionChk = userService.gridSessionChk(cmap, request);
@@ -58,8 +65,10 @@ public class KP2020PrintHistoryController {
     		model.addAttribute("printString", gridSessionChk.toJsonString());
         	return "common/commonString";
     	}
-
-    	CommonList resultList = printHistoryService.getPrintHistoryList(cmap);
+    	System.out.println(" cmap " + "  : " + cmap.toString());
+    	CommonList resultList = printHistoryService.getPrintHistoryList(cmap);    	
+    	System.out.println(" resultList " + "  : " + resultList.toString());
+    	System.out.println(" resultList.size() " + "  : " + resultList.size());
     	CommonMap result = new CommonMap();
     	result.put("resultList", resultList);
     	result.put("totalRow", resultList.totalRow);
