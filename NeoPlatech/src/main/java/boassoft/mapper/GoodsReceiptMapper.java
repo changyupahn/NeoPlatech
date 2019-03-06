37e26918a5b7b0779ccad490bd1df7ec56b0d0c9
GIGABYTE-P25 <GIGABYTE-P25@DESKTOP-0449D1S>
2019-03-06 14:00:16 +0900

    추가

---
+++ NeoPlatech/src/main/java/boassoft/mapper/GoodsReceiptMapper.java
@@ -0,0 +1,14 @@
+package boassoft.mapper;
+
+import boassoft.util.CommonList;
+import boassoft.util.CommonMap;
+import egovframework.rte.psl.dataaccess.mapper.Mapper;
+
+@Mapper("GoodsReceiptMapper")
+public interface GoodsReceiptMapper {
+	
+	public CommonList getGoodsReceiptList(CommonMap cmap) throws Exception;
+
+	public int getGoodsReceiptListCnt(CommonMap cmap) throws Exception;
+
+}
---
+++ NeoPlatech/src/main/java/boassoft/mapper/WareHouseMapper.java
@@ -0,0 +1,13 @@
+package boassoft.mapper;
+
+import boassoft.util.CommonList;
+import boassoft.util.CommonMap;
+import egovframework.rte.psl.dataaccess.mapper.Mapper;
+
+@Mapper("WareHouseMapper")
+public interface WareHouseMapper {
+
+	public CommonList getWareHouseList(CommonMap cmap) throws Exception;
+
+	public int getWareHouseListCnt(CommonMap cmap) throws Exception;
+}
