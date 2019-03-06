e0a0d8e29fc8aff930b8e1b5373eb04bce83456a
GIGABYTE-P25 <GIGABYTE-P25@DESKTOP-0449D1S>
2019-03-06 14:03:57 +0900

    추가

---
+++ NeoPlatech/src/main/java/boassoft/service/GoodsReceiptService.java
@@ -0,0 +1,9 @@
+package boassoft.service;
+
+import boassoft.util.CommonList;
+import boassoft.util.CommonMap;
+
+public interface GoodsReceiptService {
+
+	public CommonList getGoodsReceiptList(CommonMap cmap) throws Exception;
+}
---
+++ NeoPlatech/src/main/java/boassoft/service/WareHouseService.java
@@ -0,0 +1,11 @@
+package boassoft.service;
+
+import boassoft.util.CommonList;
+import boassoft.util.CommonMap;
+
+public interface WareHouseService {
+
+	public CommonList getWareHouseList(CommonMap cmap) throws Exception;
+	
+	
+}
