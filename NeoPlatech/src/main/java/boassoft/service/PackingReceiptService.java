40f12dc7ca9d48e10056bc3fcb7832ac1444fa38
GIGABYTE-P25 <GIGABYTE-P25@DESKTOP-0449D1S>
2019-03-07 19:42:37 +0900

    추가

---
+++ NeoPlatech/src/main/java/boassoft/service/PackingReceiptService.java
@@ -0,0 +1,10 @@
+package boassoft.service;
+
+import boassoft.util.CommonList;
+import boassoft.util.CommonMap;
+
+public interface PackingReceiptService {
+	
+	public CommonList getPackingReceiptList(CommonMap cmap) throws Exception;
+
+}
