55824e81d88cd72a1dde7f751896d05355c965cc
GIGABYTE-P25 <GIGABYTE-P25@DESKTOP-0449D1S>
2019-03-07 19:36:18 +0900

    추가

---
+++ NeoPlatech/src/main/java/boassoft/service/SubsiDiaryReceiptService.java
@@ -0,0 +1,9 @@
+package boassoft.service;
+
+import boassoft.util.CommonList;
+import boassoft.util.CommonMap;
+
+public interface SubsiDiaryReceiptService {
+
+	public CommonList getSubsiDiaryReceiptList(CommonMap cmap) throws Exception;
+}
---
+++ NeoPlatech/src/main/java/boassoft/service/SubsiDiaryShipmentOutService.java
@@ -0,0 +1,9 @@
+package boassoft.service;
+
+import boassoft.util.CommonList;
+import boassoft.util.CommonMap;
+
+public interface SubsiDiaryShipmentOutService {
+
+	public CommonList getSubsiDiaryShipmentOutList(CommonMap cmap) throws Exception;
+}
