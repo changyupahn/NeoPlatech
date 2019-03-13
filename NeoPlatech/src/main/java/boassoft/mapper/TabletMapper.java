7fdd8f6833698df3208d12979d77a1233900068f
GIGABYTE-P25 <GIGABYTE-P25@DESKTOP-0449D1S>
2019-03-13 14:10:58 +0900

    수정

--- NeoPlatech/src/main/java/boassoft/mapper/TabletMapper.java
+++ NeoPlatech/src/main/java/boassoft/mapper/TabletMapper.java
@@ -2,10 +2,13 @@ package boassoft.mapper;
 
 import java.util.List;
 
+import egovframework.rte.psl.dataaccess.mapper.Mapper;
 import boassoft.util.CommonMap;
 
+@Mapper("TabletMapper")
 public interface TabletMapper {
 
+	@SuppressWarnings("rawtypes")
 	public List getGoodsShipmentOutListXml(CommonMap cmap) throws Exception;
 	
 	public int getGoodsShipmentOutListXmlCnt(CommonMap cmap) throws Exception;
