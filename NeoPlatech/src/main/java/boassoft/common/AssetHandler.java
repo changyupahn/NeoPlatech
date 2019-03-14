d5bd6f5e6dd2a6593a7c7b97e01c68937c1537d5
GIGABYTE-P25 <GIGABYTE-P25@DESKTOP-0449D1S>
2019-03-14 09:40:38 +0900

    수정

--- NeoPlatech/src/main/java/boassoft/common/AssetHandler.java
+++ NeoPlatech/src/main/java/boassoft/common/AssetHandler.java
@@ -9,4 +9,39 @@ import boassoft.util.CommonMap;
 
 public class AssetHandler extends DefaultHandler{
 
+	private CommonList list = null;
+	private CommonMap cmap = null;
+	
+	public CommonList getAssetList() {        
+		return list;
+    }
+	
+	private boolean org = false;
+	private boolean asset_no = false;
+	
+	@Override    
+	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+		if (qName.equalsIgnoreCase("item"))
+    	{
+           cmap = new CommonMap();
+    		
+    		if (list == null)
+    			list = new CommonList();    	
+    	}
+		else if (qName.equalsIgnoreCase("org")){ org = true; }
+    	else if (qName.equalsIgnoreCase("asset_no")){ asset_no = true; }
+	}
+	
+	@Override    
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+    	if (qName.equalsIgnoreCase("item")) {
+    		list.add(cmap);
+        }    
+    }
+	
+	@Override    
+    public void characters(char ch[], int start, int length) throws SAXException {
+    	if (org) { cmap.put("org", new String(ch, start, length)); org= false; }
+    	if (asset_no) { cmap.put("asset_no", new String(ch, start, length)); asset_no= false; }
+    }
 }
