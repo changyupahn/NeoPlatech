20196b0854e7957187c27d9c2bf45cc2cbe4285b
GIGABYTE-P25 <GIGABYTE-P25@DESKTOP-0449D1S>
2019-03-21 19:35:59 +0900

    수정

--- NeoPlatech/src/main/java/boassoft/service/impl/GoodsReceiptServiceImpl.java
+++ NeoPlatech/src/main/java/boassoft/service/impl/GoodsReceiptServiceImpl.java
@@ -22,6 +22,36 @@ public class GoodsReceiptServiceImpl extends EgovAbstractServiceImpl implements 
 		CommonList list = goodsReceiptMapper.getGoodsReceiptList(cmap);
 		list.totalRow = goodsReceiptMapper.getGoodsReceiptListCnt(cmap);
 		return list;
+	}
+
+	@Override
+	public CommonList getOptionVendorList(CommonMap cmap) throws Exception {
+		// TODO Auto-generated method stub
+		CommonList list = goodsReceiptMapper.getOptionVendorList(cmap);
+		return list;
+	}
+
+	@Override
+	public CommonList getOptionItemList(CommonMap cmap) throws Exception {
+		// TODO Auto-generated method stub
+		CommonList list = goodsReceiptMapper.getOptionItemList(cmap);
+		return list;
+	}
+
+	@Override
+	public CommonList getOptionPNoList(CommonMap cmap) throws Exception {
+		// TODO Auto-generated method stub
+		CommonList list = goodsReceiptMapper.getOptionPNoList(cmap);
+		return list;
+	}
+
+	@Override
+	public CommonList getGoodsReceiptDetailList(CommonMap cmap)
+			throws Exception {
+		// TODO Auto-generated method stub
+		CommonList list = goodsReceiptMapper.getGoodsReceiptDetailList(cmap);
+		list.totalRow = goodsReceiptMapper.getGoodsReceiptDetailListCnt(cmap);
+		return list;
 	}	
 
 }
