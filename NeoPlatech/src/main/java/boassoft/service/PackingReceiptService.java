f58933acb2980610306086232e18066cd399a193
GIGABYTE-P25 <GIGABYTE-P25@DESKTOP-0449D1S>
2019-03-25 08:16:24 +0900

    수정

--- NeoPlatech/src/main/java/boassoft/service/PackingReceiptService.java
+++ NeoPlatech/src/main/java/boassoft/service/PackingReceiptService.java
@@ -6,5 +6,13 @@ import boassoft.util.CommonMap;
 public interface PackingReceiptService {
 	
 	public CommonList getPackingReceiptList(CommonMap cmap) throws Exception;
+	
+	public CommonList getOptionVendorList(CommonMap cmap) throws Exception;
+
+	public CommonList getOptionItemList(CommonMap cmap) throws Exception;
+
+	public CommonList getOptionPNoList(CommonMap cmap) throws Exception;
+	
+	public CommonList getPackingReceiptDetailList(CommonMap cmap) throws Exception;
 
 }
