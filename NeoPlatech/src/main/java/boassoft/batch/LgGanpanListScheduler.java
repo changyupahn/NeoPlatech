58535a61817cc96227bb9d9493d5573fdf94a157
GIGABYTE-P25 <GIGABYTE-P25@DESKTOP-0449D1S>
2019-03-27 10:05:33 +0900

    수정

--- NeoPlatech/src/main/java/boassoft/batch/LgGanpanListScheduler.java
+++ NeoPlatech/src/main/java/boassoft/batch/LgGanpanListScheduler.java
@@ -46,18 +46,11 @@ public class LgGanpanListScheduler {
 		
         System.out.println("insertBatchSchdul : " + DateUtil.getFormatDate("yyyy-MM-dd hh:mm:ss"));
 		
-		String batchId = "batch_012";
-		//String batchCycle = "0 10 4 * * ?"; //매일 새벽 4시 10분
-		//String batchCycle = "0 20 12 * * ?";
-		//String batchCycle = "10 0/1 * * * ?"; //1분 마다
-		//String batchCycle = "0 0/2 * * * ?"; //2분 마다
-		//String batchCycle = "0 0/1 * * * ?"; //10분 마다
-		//String batchCycle = "10 * * * * ?"; //1분 마다
+		String batchId = "batch_012";		
 		String batchCycle = "0 30 3 * * ?"; //매일 새벽 3시 30분
 	
         HashMap<String, Object> jobMap = new HashMap<String, Object>();
-		
-		//jobMap.put("batchService", batchService);
+				
 		jobMap.put("batchMssqlService", batchMssqlService);
 		jobMap.put("batchMysqlInterfaceService", batchMysqlInterfaceService);
 		
@@ -69,8 +62,7 @@ public class LgGanpanListScheduler {
      	jobDetail.setJobClass(LgGanpanListScriptJob.class);
      	jobDetail.setJobDataMap(jobDataMap);
      	
-    	// Trigger 만들기
-		//Trigger trigger = TriggerUtils.makeImmediateTrigger(jobDetail.getName(), 1, 1000000);
+    	// Trigger 만들기		
 		CronTrigger trigger = new CronTrigger(batchId, null, batchCycle);
 		
 		System.out.println("배치스케줄을 등록합니다. 배치스케줄ID : " + batchId );
