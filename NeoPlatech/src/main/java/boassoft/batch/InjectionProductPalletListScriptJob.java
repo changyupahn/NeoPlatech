c123f41297ec25f8ce9ba6eb08388d310182db49
GIGABYTE-P25 <GIGABYTE-P25@DESKTOP-0449D1S>
2019-03-27 10:03:48 +0900

    수정

--- NeoPlatech/src/main/java/boassoft/batch/InjectionProductPalletListScriptJob.java
+++ NeoPlatech/src/main/java/boassoft/batch/InjectionProductPalletListScriptJob.java
@@ -10,20 +10,11 @@ import org.quartz.JobExecutionException;
 
 import boassoft.service.BatchMssqlService;
 import boassoft.service.BatchMysqlInterfaceService;
-import boassoft.service.BatchService;
 import boassoft.util.CommonList;
 import boassoft.util.CommonMap;
 import boassoft.util.DateUtil;
 
 public class InjectionProductPalletListScriptJob implements Job {
-	
-	/** batchService */
-	private BatchService batchService;
-
-	/** setBatchService */
-	public void setBatchService(BatchService batchService) {
-		this.batchService = batchService;
-	}
 
 	/** batchMssqlService */
 	private BatchMssqlService batchMssqlService;
@@ -48,53 +39,52 @@ public class InjectionProductPalletListScriptJob implements Job {
 		// TODO Auto-generated method stub
 		System.out.println("egovBatchExecute() : "
 				+ DateUtil.getFormatDate("yyyy-MM-dd hh:mm:ss"));
-		
-		// setBatchService((BatchService)jobContext.getJobDetail().getJobDataMap().get("batchService"));
-				setBatchMssqlService((BatchMssqlService) jobContext.getJobDetail()
-						.getJobDataMap().get("batchMssqlService"));
-				setBatchMysqlInterfaceService((BatchMysqlInterfaceService) jobContext
-						.getJobDetail().getJobDataMap()
-						.get("batchMysqlInterfaceService"));
-		
-				try {
-					CommonMap cmap = new CommonMap();
-					
-					SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-					
-					Calendar c1 = new GregorianCalendar();
-					c1.add(Calendar.DATE, -1); // 오늘날짜로부터 -1		
 
-					String strYesterDay = sdf.format(c1.getTime()); // String으로 저장
-					
-					Calendar c2 = Calendar.getInstance();
-					String strToday = sdf.format(c2.getTime());
-					
-					cmap.put("startDate", strYesterDay);
-					//System.out.println(" row startDate  : "+ strYesterDay + " ");
-					cmap.put("endDate", strToday);
-					//System.out.println(" row endDate  : "+ strToday + " ");
-					
-					CommonList clist = new CommonList();
-					
-					batchMysqlInterfaceService.deleteInjectionProductPalletList(cmap);
-					
-					clist = batchMssqlService.getInjectionProductPalletList(cmap);
-					
-					if (!clist.isEmpty() && clist.size() > 0) {
-						for (int i = 0; i < clist.size(); i++) {
-							CommonMap gmap = clist.getMap(i);
-							
-							batchMysqlInterfaceService.insertInjectionProductPalletList(gmap);
-						}
-					}	
-				} catch (Exception e) {
-					e.printStackTrace();
+		setBatchMssqlService((BatchMssqlService) jobContext.getJobDetail()
+				.getJobDataMap().get("batchMssqlService"));
+		setBatchMysqlInterfaceService((BatchMysqlInterfaceService) jobContext
+				.getJobDetail().getJobDataMap()
+				.get("batchMysqlInterfaceService"));
+
+		try {
+			CommonMap cmap = new CommonMap();
+
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+			Calendar c1 = new GregorianCalendar();
+			c1.add(Calendar.DATE, -1); // 오늘날짜로부터 -1
+
+			String strYesterDay = sdf.format(c1.getTime()); // String으로 저장
+
+			Calendar c2 = Calendar.getInstance();
+			String strToday = sdf.format(c2.getTime());
+
+			cmap.put("startDate", strYesterDay);
+
+			cmap.put("endDate", strToday);
+
+			CommonList clist = new CommonList();
+
+			batchMysqlInterfaceService.deleteInjectionProductPalletList(cmap);
+
+			clist = batchMssqlService.getInjectionProductPalletList(cmap);
+
+			if (!clist.isEmpty() && clist.size() > 0) {
+				for (int i = 0; i < clist.size(); i++) {
+					CommonMap gmap = clist.getMap(i);
+
+					batchMysqlInterfaceService
+							.insertInjectionProductPalletList(gmap);
 				}
-				
-				// jobContext에 결과값을 저장한다.
-				jobContext.setResult(true);
-				System.out.println("egovBatchExecute() : OK "
-						+ DateUtil.getFormatDate("yyyy-MM-dd hh:mm:ss"));	
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// jobContext에 결과값을 저장한다.
+		jobContext.setResult(true);
+		System.out.println("egovBatchExecute() : OK "
+				+ DateUtil.getFormatDate("yyyy-MM-dd hh:mm:ss"));
 	}
 
 }
