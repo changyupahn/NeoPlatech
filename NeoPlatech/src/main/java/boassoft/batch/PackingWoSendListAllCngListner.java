18f13ef0f6e075f5884358cb049c016a93dea093
GIGABYTE-P25 <GIGABYTE-P25@DESKTOP-0449D1S>
2019-03-28 09:50:56 +0900

    추가

---
+++ NeoPlatech/src/main/java/boassoft/batch/PackingWoSendListAllCngListner.java
@@ -0,0 +1,57 @@
+package boassoft.batch;
+
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.quartz.JobListener;
+
+public class PackingWoSendListAllCngListner implements JobListener{
+
+	/**
+     * Job Listener 이름을 리턴한다. 
+     * @see org.quartz.JobListener#getName()
+     */
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return this.getClass().getName();
+	}
+
+	/**
+	 * Batch 작업을 실행하기전에 Batch결과 '수행중'상태로 저장한다. 
+	 * 
+	 * @param jobContext JobExecutionContext 
+	 * @see org.quartz.JobListener#jobToBeExecuted(JobExecutionContext jobContext) 
+	 */
+	@Override
+	public void jobToBeExecuted(JobExecutionContext jobContext) {
+		// TODO Auto-generated method stub
+		System.out.println("job[" + jobContext.getJobDetail().getName() + "] " + "jobToBeExecuted ");
+	}
+
+	/**
+	 * Batch 작업을 실행하기전에 Batch결과 '수행중'상태로 저장한다. 
+	 * 
+	 * @param jobContext JobExecutionContext 
+	 * @see org.quartz.JobListener#jobToBeExecuted(JobExecutionContext jobContext) 
+	 */
+	@Override
+	public void jobExecutionVetoed(JobExecutionContext jobContext) {
+		// TODO Auto-generated method stub		
+		System.out.println("job[" + jobContext.getJobDetail().getName() + "] " + "jobExecutionVetoed ");
+	}
+
+	/**
+	 * Batch 작업을 완료한후 Batch결과 '완료'상태로 저장한다. 
+	 * 
+	 * @param jobContext JobExecutionContext 
+	 * @see org.quartz.JobListener#jobWasExecuted(JobExecutionContext jobContext) 
+	 */
+	@Override
+	public void jobWasExecuted(JobExecutionContext jobContext,
+			JobExecutionException jee) {
+		// TODO Auto-generated method stub
+		System.out.println("job[" + jobContext.getJobDetail().getName() + "] " + "jobWasExecuted ");
+		System.out.println("job[" + jobContext.getJobDetail().getName() + "] " + "수행시간 : " + jobContext.getFireTime() + "," + jobContext.getJobRunTime());
+	}
+
+}
---
+++ NeoPlatech/src/main/java/boassoft/batch/PackingWoSendListAllCngScriptJob.java
@@ -0,0 +1,86 @@
+package boassoft.batch;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import boassoft.service.BatchMssqlService;
+import boassoft.service.BatchMysqlInterfaceService;
+import boassoft.util.CommonList;
+import boassoft.util.CommonMap;
+import boassoft.util.DateUtil;
+
+public class PackingWoSendListAllCngScriptJob  implements Job {
+
+	/** batchMssqlService */
+	private BatchMssqlService batchMssqlService;
+
+	/** setBatchService */
+	public void setBatchMssqlService(BatchMssqlService batchMssqlService) {
+		this.batchMssqlService = batchMssqlService;
+	}
+	
+	/** batchMysqlInterfaceService */
+	private BatchMysqlInterfaceService batchMysqlInterfaceService;
+	
+	/** setBatchService */
+	public void setBatchMysqlInterfaceService(BatchMysqlInterfaceService batchMysqlInterfaceService) {
+		this.batchMysqlInterfaceService = batchMysqlInterfaceService;
+	}
+	
+	@Override
+	public void execute(JobExecutionContext jobContext)
+			throws JobExecutionException {
+		// TODO Auto-generated method stub
+	
+		System.out.println("egovBatchExecute() : " + DateUtil.getFormatDate("yyyy-MM-dd hh:mm:ss"));
+		setBatchMssqlService((BatchMssqlService)jobContext.getJobDetail().getJobDataMap().get("batchMssqlService"));
+		setBatchMysqlInterfaceService((BatchMysqlInterfaceService)jobContext.getJobDetail().getJobDataMap().get("batchMysqlInterfaceService"));
+		
+		try {
+			
+			CommonMap cmap = new CommonMap();
+
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+			
+			Calendar c1 = new GregorianCalendar();
+			c1.add(Calendar.DATE, -1); // 오늘날짜로부터 -1	
+			
+            String strYesterDay = sdf.format(c1.getTime()); // String으로 저장
+			
+			Calendar c2 = Calendar.getInstance();
+			String strToday = sdf.format(c2.getTime());
+
+			cmap.put("startDate", strYesterDay);
+			//System.out.println(" row startDate  : "+ strYesterDay + " ");
+			cmap.put("endDate", strToday);
+			//System.out.println(" row endDate  : "+ strToday + " ");
+			
+			CommonList clist = new CommonList();
+			
+			clist =  batchMysqlInterfaceService.getPackingWoSendListAllCng(cmap);
+			
+			if (!clist.isEmpty() && clist.size() > 0) {
+				for (int i = 0; i < clist.size(); i++) {
+					
+					CommonMap gmap = clist.getMap(i);
+					batchMysqlInterfaceService.insertPackingWoSendListAllCngMStock(gmap);
+					batchMysqlInterfaceService.insertPackingWoSendListAllCngMTransaction(gmap);
+					batchMssqlService.insertPackingWoSendListAllCngInputQty(gmap);
+				}
+			}	
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		// jobContext에 결과값을 저장한다.
+				jobContext.setResult(true);
+				System.out.println("egovBatchExecute() : OK "
+						+ DateUtil.getFormatDate("yyyy-MM-dd hh:mm:ss"));
+	}
+
+}
