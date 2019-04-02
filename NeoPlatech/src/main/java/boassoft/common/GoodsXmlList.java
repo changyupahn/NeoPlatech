6b3451d8bb53c478a49810bd1749b293b7839fdb
GIGABYTE-P25 <GIGABYTE-P25@DESKTOP-0449D1S>
2019-04-02 20:03:29 +0900

    수정

--- NeoPlatech/src/main/java/boassoft/common/GoodsXmlList.java
+++ NeoPlatech/src/main/java/boassoft/common/GoodsXmlList.java
@@ -5,16 +5,20 @@ import java.util.List;
 
 import boassoft.common.GoodsXml;
 
-public class GoodsXmlList  {
+public class GoodsXmlList extends ArrayList<GoodsXml> {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8473595255765995949L;
 	private List<GoodsXml> list;
 	
 	public GoodsXmlList(){
         list = new ArrayList<GoodsXml>();
     }
 	
-	public void add(GoodsXml p){
-        list.add(p);
+	public boolean add(GoodsXml p){
+        return list.add(p);
     }
 	
 	public void addList(List<GoodsXml> l){
