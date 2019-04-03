3ef45ab9a6583b26a9a361081a19b28a06224ac2
GIGABYTE-P25 <GIGABYTE-P25@DESKTOP-0449D1S>
2019-04-03 14:40:30 +0900

    수정

--- NeoPlatech/src/main/java/boassoft/mapper/PackingShipmentOutMapper.java
+++ NeoPlatech/src/main/java/boassoft/mapper/PackingShipmentOutMapper.java
@@ -10,4 +10,10 @@ public interface PackingShipmentOutMapper {
 	public CommonList getPackingShipmentOutList(CommonMap cmap) throws Exception;
 	
 	public int getPackingShipmentOutListCnt(CommonMap cmap) throws Exception;
+
+	public int insertMOutOrderLine(CommonMap cmap) throws Exception;
+
+	public int insertMOutOrder(CommonMap cmap) throws Exception;
+
+	public int insertCInvoicePo(CommonMap cmap) throws Exception;
 }
