d3e2c04d0b2c89fe6fb2f43eabd500f5aae73ed1
GIGABYTE-P25 <GIGABYTE-P25@DESKTOP-0449D1S>
2019-04-03 19:50:11 +0900

    수정

--- NeoPlatech/src/main/java/boassoft/common/GoodsXml.java
+++ NeoPlatech/src/main/java/boassoft/common/GoodsXml.java
@@ -47,90 +47,7 @@ public class GoodsXml implements Serializable {
 	private String prim_key;
 	private String lg_line;	
 	private String pg_od_id;
-	public String getPg_od_id() {
-		return pg_od_id;
-	}
-	public void setPg_od_id(String pg_od_id) {
-		this.pg_od_id = pg_od_id;
-	}
-	public String getPkg_po_no() {
-		return pkg_po_no;
-	}
-	public void setPkg_po_no(String pkg_po_no) {
-		this.pkg_po_no = pkg_po_no;
-	}
-	public String getTool_name() {
-		return tool_name;
-	}
-	public void setTool_name(String tool_name) {
-		this.tool_name = tool_name;
-	}
-	public String getLine() {
-		return line;
-	}
-	public void setLine(String line) {
-		this.line = line;
-	}
-	public String getFinal_vendor() {
-		return final_vendor;
-	}
-	public void setFinal_vendor(String final_vendor) {
-		this.final_vendor = final_vendor;
-	}
-	public String getClazz() {
-		return clazz;
-	}
-	public void setClazz(String clazz) {
-		this.clazz = clazz;
-	}
-	public String getPart_number() {
-		return part_number;
-	}
-	public void setPart_number(String part_number) {
-		this.part_number = part_number;
-	}
-	public String getDesc() {
-		return desc;
-	}
-	public void setDesc(String desc) {
-		this.desc = desc;
-	}
-	public String getResult_qty() {
-		return result_qty;
-	}
-	public void setResult_qty(String result_qty) {
-		this.result_qty = result_qty;
-	}
-	public String getNeo_od_time() {
-		return neo_od_time;
-	}
-	public void setNeo_od_time(String neo_od_time) {
-		this.neo_od_time = neo_od_time;
-	}
-	public String getNeo_od_day() {
-		return neo_od_day;
-	}
-	public void setNeo_od_day(String neo_od_day) {
-		this.neo_od_day = neo_od_day;
-	}
-	public String getNeo_od_qty() {
-		return neo_od_qty;
-	}
-	public void setNeo_od_qty(String neo_od_qty) {
-		this.neo_od_qty = neo_od_qty;
-	}
-	public String getQty_on_hand() {
-		return qty_on_hand;
-	}
-	public void setQty_on_hand(String qty_on_hand) {
-		this.qty_on_hand = qty_on_hand;
-	}
-	public String getPre_qty_on_hand() {
-		return pre_qty_on_hand;
-	}
-	public void setPre_qty_on_hand(String pre_qty_on_hand) {
-		this.pre_qty_on_hand = pre_qty_on_hand;
-	}
+	private String od_qty_id;			
 	private String pkg_po_no;
     private String tool_name;
     private String line;
@@ -143,9 +60,37 @@ public class GoodsXml implements Serializable {
     private String neo_od_day;
     private String neo_od_qty;
     private String qty_on_hand;
-    private String pre_qty_on_hand;
+    private String pre_qty_on_hand;   
+	private String draw_no;
+	private String row_num;
+	private String calcu_true;
+	private String demand_floor;
+	private String rsltdate;	
+	private String rsltstate;
+	private String rslt_user_no;
+	private String readdate;
+	private String readstate;
+	private String read_user_no;	
+			
 	
-	
+	public String getDemand_floor() {
+		return demand_floor;
+	}
+	public void setDemand_floor(String demand_floor) {
+		this.demand_floor = demand_floor;
+	}
+	public String getCalcu_true() {
+		return calcu_true;
+	}
+	public void setCalcu_true(String calcu_true) {
+		this.calcu_true = calcu_true;
+	}
+	public String getRow_num() {
+		return row_num;
+	}
+	public void setRow_num(String row_num) {
+		this.row_num = row_num;
+	}
 	public String getPt_od_id() {
 		return pt_od_id;
 	}
@@ -368,10 +313,138 @@ public class GoodsXml implements Serializable {
 	public void setLg_line(String lg_line) {
 		this.lg_line = lg_line;
 	}
-	
-	
-	
-
+	public String getPg_od_id() {
+		return pg_od_id;
+	}
+	public void setPg_od_id(String pg_od_id) {
+		this.pg_od_id = pg_od_id;
+	}
+	public String getPkg_po_no() {
+		return pkg_po_no;
+	}
+	public void setPkg_po_no(String pkg_po_no) {
+		this.pkg_po_no = pkg_po_no;
+	}
+	public String getTool_name() {
+		return tool_name;
+	}
+	public void setTool_name(String tool_name) {
+		this.tool_name = tool_name;
+	}
+	public String getLine() {
+		return line;
+	}
+	public void setLine(String line) {
+		this.line = line;
+	}
+	public String getFinal_vendor() {
+		return final_vendor;
+	}
+	public void setFinal_vendor(String final_vendor) {
+		this.final_vendor = final_vendor;
+	}
+	public String getClazz() {
+		return clazz;
+	}
+	public void setClazz(String clazz) {
+		this.clazz = clazz;
+	}
+	public String getPart_number() {
+		return part_number;
+	}
+	public void setPart_number(String part_number) {
+		this.part_number = part_number;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	public String getResult_qty() {
+		return result_qty;
+	}
+	public void setResult_qty(String result_qty) {
+		this.result_qty = result_qty;
+	}
+	public String getNeo_od_time() {
+		return neo_od_time;
+	}
+	public void setNeo_od_time(String neo_od_time) {
+		this.neo_od_time = neo_od_time;
+	}
+	public String getNeo_od_day() {
+		return neo_od_day;
+	}
+	public void setNeo_od_day(String neo_od_day) {
+		this.neo_od_day = neo_od_day;
+	}
+	public String getNeo_od_qty() {
+		return neo_od_qty;
+	}
+	public void setNeo_od_qty(String neo_od_qty) {
+		this.neo_od_qty = neo_od_qty;
+	}
+	public String getQty_on_hand() {
+		return qty_on_hand;
+	}
+	public void setQty_on_hand(String qty_on_hand) {
+		this.qty_on_hand = qty_on_hand;
+	}
+	public String getPre_qty_on_hand() {
+		return pre_qty_on_hand;
+	}
+	public void setPre_qty_on_hand(String pre_qty_on_hand) {
+		this.pre_qty_on_hand = pre_qty_on_hand;
+	}
+	public String getOd_qty_id() {
+		return od_qty_id;
+	}
+	public void setOd_qty_id(String od_qty_id) {
+		this.od_qty_id = od_qty_id;
+	}
+	public String getDraw_no() {
+		return draw_no;
+	}
+	public void setDraw_no(String draw_no) {
+		this.draw_no = draw_no;
+	}
+	public String getRsltdate() {
+		return rsltdate;
+	}
+	public void setRsltdate(String rsltdate) {
+		this.rsltdate = rsltdate;
+	}
+	public String getRsltstate() {
+		return rsltstate;
+	}
+	public void setRsltstate(String rsltstate) {
+		this.rsltstate = rsltstate;
+	}
+	public String getRslt_user_no() {
+		return rslt_user_no;
+	}
+	public void setRslt_user_no(String rslt_user_no) {
+		this.rslt_user_no = rslt_user_no;
+	}
+	public String getReaddate() {
+		return readdate;
+	}
+	public void setReaddate(String readdate) {
+		this.readdate = readdate;
+	}
+	public String getReadstate() {
+		return readstate;
+	}
+	public void setReadstate(String readstate) {
+		this.readstate = readstate;
+	}
+	public String getRead_user_no() {
+		return read_user_no;
+	}
+	public void setRead_user_no(String read_user_no) {
+		this.read_user_no = read_user_no;
+	}
 	
 	
 }
