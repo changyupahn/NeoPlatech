0281d3d9928e33257f89f0093ff1add4051d0a6f
GIGABYTE-P25 <GIGABYTE-P25@DESKTOP-0449D1S>
2019-04-03 14:19:04 +0900

    수정

--- NeoPlatech/src/main/java/boassoft/service/impl/TabletServiceImpl.java
+++ NeoPlatech/src/main/java/boassoft/service/impl/TabletServiceImpl.java
@@ -61,7 +61,7 @@ public class TabletServiceImpl extends EgovAbstractServiceImpl implements Tablet
 		}
 	}
 
-	@SuppressWarnings("unchecked")
+	
 	@Override
 	public GoodsXmlList getPackingReceptListXml(CommonMap cmap)
 			throws Exception {
@@ -78,6 +78,24 @@ public class TabletServiceImpl extends EgovAbstractServiceImpl implements Tablet
 		GoodsXmlList list = new GoodsXmlList();
 		list.addList( (List)tabletMapper.getPackingReceptListXml(cmap) );
 		
+		return list;
+	}
+
+	@Override
+	public GoodsXmlList getPackingShipmentOutListXml(CommonMap cmap)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+		cmap.put("dataOrder", cmap.getString("dataOrder").replaceAll("[^0-9a-zA-Z_.]",""));
+    	cmap.put("dataOrderArrow", cmap.getString("dataOrderArrow", "asc").toLowerCase().replaceAll("^(asc|desc)$","$1"));
+		
+    	cmap.put("pageLimit", (cmap.getInt("pageIdx") - 1) * cmap.getInt("pageSize",10));
+		cmap.put("pageSize", cmap.getString("pageSize","10"));
+		cmap.put("pageStartNum", (cmap.getInt("pageIdx") - 1) * cmap.getInt("pageSize") + 1 + "");
+		cmap.put("pageEndNum", cmap.getInt("pageIdx") * cmap.getInt("pageSize") + "");
+		
+		GoodsXmlList list = new GoodsXmlList();
+		list.addList( (List)tabletMapper.getPackingShipmentOutListXml(cmap) );
 		return list;
 	}
 
