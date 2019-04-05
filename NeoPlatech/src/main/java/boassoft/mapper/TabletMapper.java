1ea2b12b2d82a52a0f8d405cf3c63cd1d962bc73
GIGABYTE-P25 <GIGABYTE-P25@DESKTOP-0449D1S>
2019-04-05 14:15:27 +0900

    수정

--- NeoPlatech/src/main/java/boassoft/mapper/TabletMapper.java
+++ NeoPlatech/src/main/java/boassoft/mapper/TabletMapper.java
@@ -3,7 +3,7 @@ package boassoft.mapper;
 import java.util.List;
 
 import egovframework.rte.psl.dataaccess.mapper.Mapper;
-import boassoft.common.GoodsXmlList;
+import boassoft.common.GoodsXml;
 import boassoft.util.CommonList;
 import boassoft.util.CommonMap;
 
@@ -11,22 +11,26 @@ import boassoft.util.CommonMap;
 public interface TabletMapper {
 
 	
-	public List getGoodsShipmentOutListXml(CommonMap cmap) throws Exception;
+	public List<GoodsXml> getGoodsShipmentOutListXml(CommonMap cmap) throws Exception;
 	
 	public int getGoodsShipmentOutListXmlCnt(CommonMap cmap) throws Exception;
 
 	public void updateIGoodsShipmentOut(CommonMap cmap) throws Exception;
 	
-	public GoodsXmlList getPackingReceptListXml(CommonMap cmap) throws Exception;
+	public List<GoodsXml> getPackingReceptListXml(CommonMap cmap) throws Exception;
 
 	public CommonList getPackingShipmentOutListXml(CommonMap cmap) throws Exception;
 
-	public List getGoodsShipmentDetailOutXml(CommonMap cmap) throws Exception;
+	public List<GoodsXml> getGoodsShipmentDetailOutXml(CommonMap cmap) throws Exception;
 
-	public List goodsShipmentDetailKitItemOutXml(CommonMap cmap) throws Exception;
+	public List<GoodsXml> goodsShipmentDetailKitItemOutXml(CommonMap cmap) throws Exception;
 
-	public List goodsShipmentDetailRefItemOutXml(CommonMap cmap) throws Exception;
+	public List<GoodsXml> goodsShipmentDetailRefItemOutXml(CommonMap cmap) throws Exception;
 
-	public List optionVendorListXml(CommonMap cmap) throws Exception;
+	public List<GoodsXml> optionVendorListXml(CommonMap cmap) throws Exception;
+
+	public List<GoodsXml> optionPartNoListXml(CommonMap cmap) throws Exception;
+
+	public CommonList getPackingShipmentOutDetailListXml(CommonMap cmap) throws Exception;
 
 }
