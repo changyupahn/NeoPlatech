1ebbf64eb52bcac6d1c491e7b4bd677456bc4cd7
GIGABYTE-P25 <GIGABYTE-P25@DESKTOP-0449D1S>
2019-04-08 09:00:15 +0900

    수정

--- NeoPlatech/src/main/java/boassoft/service/GoodsShipmentOutService.java
+++ NeoPlatech/src/main/java/boassoft/service/GoodsShipmentOutService.java
@@ -8,4 +8,8 @@ public interface GoodsShipmentOutService {
 	public CommonList GoodsShipmentOutList(CommonMap cmap) throws Exception;
 		
 	public CommonList getGoodsShipmentOutDetailList(CommonMap cmap) throws Exception;
+
+	public int insertRridMOut(CommonMap gmap) throws Exception;
+
+	public int insertRridMOutLine(CommonMap gmap) throws Exception;
 }
