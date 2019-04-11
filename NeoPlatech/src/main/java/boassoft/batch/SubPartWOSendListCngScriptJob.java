95639a25b955ead369281a2aec5643351169fa9a
GIGABYTE-P25 <GIGABYTE-P25@DESKTOP-0449D1S>
2019-04-11 17:17:30 +0900

    수정

--- NeoPlatech/src/main/java/boassoft/batch/SubPartWOSendListCngScriptJob.java
+++ NeoPlatech/src/main/java/boassoft/batch/SubPartWOSendListCngScriptJob.java
@@ -73,9 +73,48 @@ public class SubPartWOSendListCngScriptJob implements Job{
 					
 					CommonMap gmap = clist.getMap(i);
 					
+					System.out.println(" OD_ID " + " : " + i + " + 번째 " + " : " + gmap.getString("odId"));
+					System.out.println(" DEMAND_ID " + " : " + i + " + 번째 " + " : " + gmap.getString("demandId"));
+					System.out.println(" LGE_DATE " + " : " + i + " + 번째 " + " : " + gmap.getString("lgeDate"));
+					System.out.println(" NEO_DATE " + " : " + i + " + 번째 " + " : " + gmap.getString("neoDate"));
+					System.out.println(" GAP_DAY " + " : " + i + " + 번째 " + " : " + gmap.getString("gayDay"));
+					System.out.println(" LG_LINE " + " : " + i + " + 번째 " + " : " + gmap.getString("lgLine"));
+					System.out.println(" TOOL " + " : " + i + " + 번째 " + " : " + gmap.getString("tool"));
+					System.out.println(" M_PART_NO " + " : " + i + " + 번째 " + " : " + gmap.getString("mPartNo"));
+					System.out.println(" PLAN_QTY " + " : " + i + " + 번째 " + " : " + gmap.getString("planQty"));
+					System.out.println(" LGM_PART_NAME " + " : " + i + " + 번째 " + " : " + gmap.getString("lgePartName"));
+					
+					System.out.println(" SUB_PART_NO " + " : " + i + " + 번째 " + " : " + gmap.getString("subPartNo"));
+					System.out.println(" SUB_PART_NAME " + " : " + i + " + 번째 " + " : " + gmap.getString("sbuPartName"));
+					System.out.println(" BOM_QTY " + " : " + i + " + 번째 " + " : " + gmap.getString("bomQty"));
+					System.out.println(" SUM_QTY " + " : " + i + " + 번째 " + " : " + gmap.getString("sumQty"));
+					
+					System.out.println(" SUM_QTY_CNG " + " : " + i + " + 번째 " + " : " + gmap.getString("sumQthCng"));
+					System.out.println(" UNIT " + " : " + i + " + 번째 " + " : " + gmap.getString("unit"));
+					System.out.println(" VENDOR " + " : " + i + " + 번째 " + " : " + gmap.getString("vendor"));
+					System.out.println(" OSP " + " : " + i + " + 번째 " + " : " + gmap.getString("osp"));
+					System.out.println(" OUT_PLACE " + " : " + i + " + 번째 " + " : " + gmap.getString("outPlace"));
+					System.out.println(" CHK_DAY " + " : " + i + " + 번째 " + " : " + gmap.getString("chkDay"));
+					System.out.println(" IN_DATE " + " : " + i + " + 번째 " + " : " + gmap.getString("inDate"));
+					System.out.println(" QTY_ON_HAND " + " : " + i + " + 번째 " + " : " + gmap.getString("qtyOnHand"));
+					System.out.println(" PRE_QTY_ON_HAND " + " : " + i + " + 번째 " + " : " + gmap.getString("preQtyOnHand"));
+					System.out.println(" QTYINVOICED " + " : " + i + " + 번째 " + " : " + gmap.getString("qtyinvoiced"));
+					
+					if(("".equals(gmap.getString("qtyOnHand")) ||("0".equals(gmap.getString("qtyOnHand"))))){
+						int qtyOnHand = gmap.getInt("preQtyOnHand") - gmap.getInt("qtyinvoiced");
+						System.out.println(" 1111 qtyOnHand" + qtyOnHand);
+						gmap.put("qtyOnHand",qtyOnHand);
+					}
+					batchMysqlInterfaceService.updateSubPartOdOnlyQtyOnHand(gmap);
+					System.out.println(" gmap insertSubPartWoSendListOdOnlyMStock " + " : " + i + " + 번째 " + " : " + " START " );
 					batchMysqlInterfaceService.insertSubPartWoSendListOdOnlyMStock(gmap);
+					System.out.println(" gmap insertSubPartWoSendListOdOnlyMStock " + " : " + i + " + 번째 " + " : " + " END " );
+					System.out.println(" gmap insertSubPartWoSendListOdOnlyMTransaction " + " : " + i + " + 번째 " + " : " + " START " );
 					batchMysqlInterfaceService.insertSubPartWoSendListOdOnlyMTransaction(gmap);
+					System.out.println(" gmap insertSubPartWoSendListOdOnlyMTransaction " + " : " + i + " + 번째 " + " : " + " END " );
+					System.out.println(" gmap insertSubPartWoSendListOdOnlyInputQty " + " : " + i + " + 번째 " + " : " + " START " );
 					batchMssqlService.insertSubPartWoSendListOdOnlyInputQty(gmap);
+					System.out.println(" gmap insertSubPartWoSendListOdOnlyInputQty " + " : " + i + " + 번째 " + " : " + " END " );
 				}
 			}
 			} catch (Exception e) {
